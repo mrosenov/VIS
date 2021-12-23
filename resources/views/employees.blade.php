ab4b5bde641c1e0b9f211ebdc06d80c4aacbf1d3
mrosenov <mitkorosenov@live.com>
2021-12-23 19:23:34 +0200

    Initial commit

---
+++ resources/views/employees.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>ВИС</title>
+</head>
+<body>
+<header>
+    <h1 style="text-align: center">ВИС: ВОЕННА ИНФОРМАЦИОННА СИСТЕМА</h1>
+    <h3 style="text-align: center">ЗА УПРАВЛЕНИЕ НА ДАННИ</h3>
+</header>
+<section>
+    <ul class="navbar">
+        <li><a href="index">Начало</a></li>
+        <li><a href="employees">Личен Състав</a></li>
+    </ul>
+</section>
+
+</body>
+</html>
